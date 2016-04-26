f3d2842f87b6ccbf0693d2e494d54f20229936a3
Joel Jayson Clark <jc214809@ohio.edu>
2016-04-25 21:03:37 -0400

    gets both team live rosters

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
    jQuery.ajaxPrefilter(function(options) {
-       if (options.crossDomain && jQuery.support.cors) {
-           options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-       }
+     if (options.crossDomain && jQuery.support.cors) {
+       options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+     }
    });
 
    function weekOfYear(date) {
-       var d = new Date(+date);
-       d.setHours(0, 0, 0);
-       d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-       return Math.ceil((((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7);
+     var d = new Date(+date);
+     d.setHours(0, 0, 0);
+     d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+     return Math.ceil((((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7);
    };
 
    angular.module('fantasyBaseball', [
-           'ngRoute',
-           'fantasyBaseball.scoreboard',
-           'fantasyBaseball.weeklyScoreBoard'
-       ])
-       .config(function myAppConfig($routeProvider, $httpProvider, $locationProvider) {
-           $routeProvider
-               .when('/', {
-                   controller: 'ScoreBoardCtrl',
-                   templateUrl: 'scoreBoard/scoreBoard.html',
-                   pageTitle: 'ScoreBoard'
-               })
-               .when('/login', {
-                   controller: 'weeklyScoreBoardCtrl',
-                   templateUrl: 'weeklyScoreboard/weeklyScoreBoard.html',
-                   pageTitle: 'weeklyScoreBoard'
-               });
-       })
-       .controller('AppCtrl', function AppCtrl($scope, $location) {
-           $scope.periodId = weekOfYear(new Date) - 3;
-           $scope.teamID = 85827;
-           $scope.leagueID = 9518;
-           //League Specific Variables above
+       'ngRoute',
+       'fantasyBaseball.scoreboard',
+       'fantasyBaseball.weeklyScoreBoard'
+     ])
+     .config(function myAppConfig($routeProvider, $httpProvider, $locationProvider) {
+       $routeProvider
+         .when('/', {
+           controller: 'ScoreBoardCtrl',
+           templateUrl: 'scoreBoard/scoreBoard.html',
+           pageTitle: 'ScoreBoard'
+         })
+         .when('/login', {
+           controller: 'weeklyScoreBoardCtrl',
+           templateUrl: 'weeklyScoreboard/weeklyScoreBoard.html',
+           pageTitle: 'weeklyScoreBoard'
+         });
+     })
+     .controller('AppCtrl', function AppCtrl($scope, $location) {
+       $scope.periodId = weekOfYear(new Date) - 3;
+       $scope.teamID = 85827;
+       $scope.leagueID = 9518;
+       //League Specific Variables above
 
-           $scope.$on('$routeChangeSuccess', function(e, nextRoute) {
-               if (nextRoute.$$route && angular.isDefined(nextRoute.$$route.pageTitle)) {
-                   $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Fantasy Baseball';
-               }
-           });
+       $scope.$on('$routeChangeSuccess', function(e, nextRoute) {
+         if (nextRoute.$$route && angular.isDefined(nextRoute.$$route.pageTitle)) {
+           $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Fantasy Baseball';
+         }
        });
+     });
--- scoreBoard/scoreBoard.js
+++ scoreBoard/scoreBoard.js
@@ -1,27 +1,111 @@
-    angular.module('fantasyBaseball.scoreboard', ['fantasyBaseball.weeklyScoreBoard'])
+    angular.module('fantasyBaseball.scoreboard', ['fantasyBaseball.weeklyScoreBoard']).directive('lineup', function() {
+        return {
+          restrict: 'E',
+          controller: 'ScoreBoardCtrl',
+          scope: {
+            startingPlayers: '=starters',
+            benchPlayers: '=bench'
+          },
+          templateUrl: 'scoreBoard/lineup.html'
+        };
+      })
       .controller('ScoreBoardCtrl', function ScoreboardController($scope, $http, $q) {
         $.ajax({
-          url: 'http://www.mlb.com/fantasylookup/json/named.fb_team_lineup.bam?period_id=' + $scope.periodId + '&team_id=' + $scope.teamID,
+          url: 'http://www.mlb.com/fantasylookup/json/named.fb_index_schedule.bam?league_id=' + $scope.leagueID,
           type: 'GET',
           dataType: 'json',
           error: function() {
             alert("Error");
           },
           success: function(data) {
-            $scope.startingPlayers = [];
-            $scope.benchPlayers = [];
-            $scope.pitchingStaff = 'lan';
-            angular.forEach(data.fb_team_lineup.queryResults.row, function(player) {
-              if (player.slot_val != 'Bn' && player.slot_val != 'DL' && player.slot_val != 'PS') {
-                $scope.startingPlayers.push(player);
-              }
-              if (player.slot_val == 'Bn') {
-                $scope.benchPlayers.push(player);
+            $scope.schedule = [];
+            $scope.matchUpIds = [];
+            $scope.opponentsSchedule = [];
+            $scope.masterSchedule = data.fb_index_schedule.queryResults;
+            angular.forEach($scope.masterSchedule.row, function(schedule) {
+              if (schedule.team_id == $scope.teamID) {
+                $scope.schedule.push(schedule);
+                $scope.matchUpIds.push(schedule.matchup_set);
               }
             });
+
+            for (var i = 0; i < $scope.masterSchedule.row.length; i++) {
+              if ($scope.masterSchedule.row[i].team_id != $scope.teamID && $scope.matchUpIds.indexOf($scope.masterSchedule.row[i].matchup_set) != -1) {
+                $scope.opponentsSchedule.push($scope.masterSchedule.row[i]);
+              }
+            }
+            $scope.homeTeam = null;
+            $scope.awayTeam = null;
+            for (var i = 0; i < $scope.schedule.length; i++) {
+              if ($scope.schedule[i].period_id == $scope.periodId) {
+                if ($scope.schedule[i].is_home == 'y') {
+                  $scope.homeTeam = $scope.schedule[i];
+                } else {
+                  $scope.awayTeam = $scope.schedule[i];
+                }
+                break;
+              }
+            }
+            for (var i = 0; i < $scope.opponentsSchedule.length; i++) {
+              if ($scope.opponentsSchedule[i].period_id == $scope.periodId) {
+                if ($scope.opponentsSchedule[i].is_home == 'y') {
+                  $scope.homeTeam = $scope.opponentsSchedule[i];
+                } else {
+                  $scope.awayTeam = $scope.opponentsSchedule[i];
+                }
+                break;
+              }
+            }
+            $scope.getLineups();
             $scope.$apply();
           }
         });
+        $scope.getLineups = function() {
+          $.ajax({
+            url: 'http://www.mlb.com/fantasylookup/json/named.fb_team_lineup.bam?period_id=' + $scope.periodId + '&team_id=' + $scope.awayTeam.team_id,
+            type: 'GET',
+            dataType: 'json',
+            error: function() {
+              alert("Joel Error");
+            },
+            success: function(data) {
+              $scope.awayStartingPlayers = [];
+              $scope.awayBenchPlayers = [];
+              $scope.pitchingStaff = 'lan';
+              angular.forEach(data.fb_team_lineup.queryResults.row, function(player) {
+                if (player.slot_val != 'Bn' && player.slot_val != 'DL' && player.slot_val != 'PS') {
+                  $scope.awayStartingPlayers.push(player);
+                }
+                if (player.slot_val == 'Bn') {
+                  $scope.awayBenchPlayers.push(player);
+                }
+              });
+              $scope.$apply();
+            }
+          });
+          $.ajax({
+            url: 'http://www.mlb.com/fantasylookup/json/named.fb_team_lineup.bam?period_id=' + $scope.periodId + '&team_id=' + $scope.homeTeam.team_id,
+            type: 'GET',
+            dataType: 'json',
+            error: function() {
+              alert("Joel Error");
+            },
+            success: function(data) {
+              $scope.homeStartingPlayers = [];
+              $scope.homeBenchPlayers = [];
+              $scope.pitchingStaff = 'lan';
+              angular.forEach(data.fb_team_lineup.queryResults.row, function(player) {
+                if (player.slot_val != 'Bn' && player.slot_val != 'DL' && player.slot_val != 'PS') {
+                  $scope.homeStartingPlayers.push(player);
+                }
+                if (player.slot_val == 'Bn') {
+                  $scope.homeBenchPlayers.push(player);
+                }
+              });
+              $scope.$apply();
+            }
+          });
+        };
         $scope.allGames = [];
         $scope.gameURLs = [];
         $scope.scoreBoard = 'http://gd2.mlb.com/components/game/mlb/year_' + "2016" + '/month_' + "04" + '/day_' + "21" + '/master_scoreboard.json';
