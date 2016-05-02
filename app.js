3b06d52dd4bd5c856f790bdfa3bc9bb05db3d62a
Joel Clark <jc214809@ohio.edu>
2016-05-02 13:22:02 -0400

    uusing my own Cors anywhere

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
    jQuery.ajaxPrefilter(function(options) {
      if (options.crossDomain && jQuery.support.cors) {
-       options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+       options.url = 'http://mlbfantasy.herokuapp.com/' + options.url;
      }
    });
 
