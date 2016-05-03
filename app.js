7c511e6261182f5e272c88d81fc0aef51d663e60
Joel Jayson Clark <jc214809@ohio.edu>
2016-05-03 12:02:57 -0400

    added blink

--- app.js
+++ app.js
@@ -11,6 +11,20 @@
      return Math.ceil((((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7);
    };
 
+   function blink() {
+     var blinks = document.getElementsByTagName('blink');
+     for (var i = blinks.length - 1; i >= 0; i--) {
+       var s = blinks[i];
+       s.style.visibility = (s.style.visibility === 'visible') ? 'hidden' : 'visible';
+     }
+     window.setTimeout(blink, 1000);
+   }
+
+   if (document.addEventListener) document.addEventListener("DOMContentLoaded", blink, false);
+   else if (window.addEventListener) window.addEventListener("load", blink, false);
+   else if (window.attachEvent) window.attachEvent("onload", blink);
+   else window.onload = blink;
+
    angular.module('fantasyBaseball', [
        'ngRoute',
        'fantasyBaseball.scoreboard',
