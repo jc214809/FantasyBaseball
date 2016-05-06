1ae7423a8f5c352828aba06254a117e7040d1623
Joel Jayson Clark <jc214809@ohio.edu>
2016-05-06 08:32:24 -0400

    trying new cors server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
    jQuery.ajaxPrefilter(function(options) {
      if (options.crossDomain && jQuery.support.cors) {
-       options.url = 'http://jc214809.herokuapp.com/' + options.url;
+       options.url = 'http://cors-anytime.herokuapp.com/' + options.url;
      }
    });
 
