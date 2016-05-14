2772b1f99c659d7612e9eadfc2a74ca30dc26c4f
Joel Clark <jc214809@ohio.edu>
2016-05-14 11:04:40 -0400

    Changed date to today

--- scoreBoard/scoreBoard.js
+++ scoreBoard/scoreBoard.js
@@ -62,7 +62,7 @@
           $scope.staffsToAdd = [];
           $scope.gameURLs = [];
           $scope.matchup = null;
-          $scope.scoreBoard = 'http://gd2.mlb.com/components/game/mlb/year_' + "2016" + '/month_' + "05" + '/day_' + "13" + '/master_scoreboard.json';
+          $scope.scoreBoard = 'http://gd2.mlb.com/components/game/mlb/year_' + "2016" + '/month_' + "05" + '/day_' + "14" + '/master_scoreboard.json';
           $http.get($scope.scoreBoard).success(function(data) {
             $scope.eachGame = data.data.games.game;
             angular.forEach($scope.eachGame, function(game) {
@@ -403,7 +403,7 @@
           $scope.playersOnDeck = [];
           $scope.playersInTheHole = [];
           $scope.allGamesDetails = [];
-          $scope.scoreBoard = 'http://gd2.mlb.com/components/game/mlb/year_' + "2016" + '/month_' + "05" + '/day_' + "13" + '/master_scoreboard.json';
+          $scope.scoreBoard = 'http://gd2.mlb.com/components/game/mlb/year_' + "2016" + '/month_' + "05" + '/day_' + "14" + '/master_scoreboard.json';
           $http.get($scope.scoreBoard).success(function(data) {
             $scope.eachGame = data.data.games.game;
             angular.forEach($scope.eachGame, function(game) {
